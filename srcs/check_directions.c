6e2d13cf6332371044d12894977cc40861ee0810
Ahlem NOURI <anouri@student.42.fr>
2024-02-01 13:48:42 +0100

    space resolved

--- srcs/check_directions.c
+++ srcs/check_directions.c
@@ -1,39 +1,48 @@
 #include "../includes/game.h"
 
-int	directionExist(char *direction, char *line, bool exist)
+static bool	NO = 0;
+static bool	SO = 0;
+static bool	WE = 0;
+static bool	EA = 0;
+
+int	directionExist(char *direction, char *line, bool *exist)
 {
+	// printf("exist = %d\n", *exist);
 	if (!ft_strncmp(direction, line, 3))
 	{
-		if (exist)
+		if (!(*exist))
+		{
+			*exist = 1;
+			printf("%s FOUND\n", direction);
+			return (0);
+		}
+		else 
 		{
 			ft_error("Invalid map (double attribution of texture)");
 			return (1);
 		}
-		exist = 1;
-		printf("%s FOUND\n", direction);
-		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 bool	checkDirection(char *line)
 {
-	static bool	NO;
-	static bool	SO;
-	static bool	WE;
-	static bool	EA;
+	// static bool	NO;
+	// static bool	SO;
+	// static bool	WE;
+	// static bool	EA;
 
-	NO = 0;
-	SO = 0;
-	WE = 0;
-	EA = 0;
-	if (!directionExist("NO", line, NO) && !directionExist("SO", line, SO)
-		&& !directionExist("EA", line, EA) && !directionExist("WE", line, WE))
-		return (0);
-	else
+
+	printf("\nno = %d, so = %d, we = %d, ea= %d\n", NO, SO, WE, EA);
+	printf("line = %s\n", line);
+	if (!ft_strncmp("NO ", line, 3) || !ft_strncmp("SO ", line, 3) || !ft_strncmp("WE ", line, 3) || !ft_strncmp("EA ", line, 3))
 	{
-		ft_error("Invalid map (Unknown direction)");
-		return (1);
+		if (!directionExist("NO", line, &NO) || !directionExist("SO", line, &SO)
+			|| !!directionExist("EA", line, &EA) || !directionExist("WE", line, &WE))
+			return(0);
 	}
-	return (0);
+	ft_error("Invalid map (Unknown direction)");
+	return (1);
+
 }
+
